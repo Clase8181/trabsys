c060c4a7fe4c920808f83da46d08142d0b9656a2
emilio9999 <31928881+emilio9999@users.noreply.github.com>
2017-09-14 09:50:22 -0500

    class arquitecto emilio

--- src/trabajogrupo/Arquitecto.java
+++ src/trabajogrupo/Arquitecto.java
@@ -1,53 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabajogrupo;
 
-/**
- *
- * @author Karen
- */
+
 public class Arquitecto {
+   
     private String lugarTrabajo;
     private String titulo;
-    private String Profesion;
-    public Arquitecto(){
-}
-
-    public Arquitecto(String lugarTrabajo) {
-        this.lugarTrabajo = lugarTrabajo;
-    }
-
-    public Arquitecto(String lugarTrabajo, String titulo, String Profesion) {
-        this.lugarTrabajo = lugarTrabajo;
-        this.titulo = titulo;
-        this.Profesion = Profesion;
-    }
-
-    public String getLugarTrabajo() {
-        return lugarTrabajo;
-    }
-
-    public void setLugarTrabajo(String lugarTrabajo) {
-        this.lugarTrabajo = lugarTrabajo;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getProfesion() {
-        return Profesion;
-    }
-
-    public void setProfesion(String Profesion) {
-        this.Profesion = Profesion;
-    }
+    private String profesion;
     
+    
+public Arquitecto(){
 }
