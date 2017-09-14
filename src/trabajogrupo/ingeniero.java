cad32c6281a24021789a447a46a213be52cd8411
emilio9999 <31928881+emilio9999@users.noreply.github.com>
2017-09-14 09:32:46 -0500

    clase ingeniero repetido emilio

---
+++ src/trabajogrupo/ingeniero.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajogrupo;
+
+/**
+ *
+ * @author lab 13
+ */
+public class ingeniero {
+    
+    private String lugarTrabajo;
+    private String titulo; 
+    private String profecion;
+    
+    public ingeniero (){
+        
+        
+        
+    }
+    
+}
