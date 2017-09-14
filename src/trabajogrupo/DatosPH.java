f87ff8854034db4c118c82e419daa61ac57e36ec
wjrc120497 <31928575+wjrc120497@users.noreply.github.com>
2017-09-14 09:51:58 -0500

    Realice mi clase DatosPh. Realizado Por Wellington Joel Rivera

---
+++ src/trabajogrupo/DatosPH.java
@@ -0,0 +1,50 @@
+
+package trabajogrupo;
+
+public class DatosPH {
+    protected String Hobbi ="Video Juegos";
+    protected String Peso ="120.gr";
+     public DatosPH(){
+    }
+    public DatosPH(String ObtenerHobbie, String ObtenerPeso){
+  
+  Hobbi=ObtenerHobbie;
+  Peso=ObtenerPeso;
+  } 
+    public static void main(String[] args) {
+         DatosPH datH=new DatosPH(); 
+         DatosPH datP=new DatosPH(); 
+         //
+    
+   System.out.println("Este es Hobbie: "+datH.getHobbi()+" Este es el peso: "+datP.getPeso());
+    }
+
+    /**
+     * @return the Hobbi
+     */
+    private String getHobbi() {
+        return Hobbi;
+    }
+
+    /**
+     * @param Hobbi the Hobbi to set
+     */
+    private void setHobbi(String Hobbi) {
+        this.Hobbi = Hobbi;
+    }
+
+    /**
+     * @return the Peso
+     */
+    private String getPeso() {
+        return Peso;
+    }
+
+    /**
+     * @param Peso the Peso to set
+     */
+    private void setPeso(String Peso) {
+        this.Peso = Peso;
+    }
+}
+
