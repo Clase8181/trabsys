eaea608412e9c9cb452b5bc442fffe5b987d1ce5
emilio9999 <31928881+emilio9999@users.noreply.github.com>
2017-09-14 09:54:26 -0500

    class casi completo arquitecto emilio

--- src/trabajogrupo/Arquitecto.java
+++ src/trabajogrupo/Arquitecto.java
@@ -10,4 +10,41 @@ public class Arquitecto {
     
     
 public Arquitecto(){
+
+
+
+
+
+
 }
+
+    public Arquitecto(String lugarTrabajo, String titulo, String profesion) {
+        this.lugarTrabajo = lugarTrabajo;
+        this.titulo = titulo;
+        this.profesion = profesion;
+    }
+
+    public String getLugarTrabajo() {
+        return lugarTrabajo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setLugarTrabajo(String lugarTrabajo) {
+        this.lugarTrabajo = lugarTrabajo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+    }
