9e61d4f3b66867ec116b1f6991faa4d1a78f802f
wjrc120497 <31928575+wjrc120497@users.noreply.github.com>
2017-09-25 13:28:11 -0500

    echor por joel rivera

---
+++ src/humano.java
@@ -0,0 +1,48 @@
+ class humano {
+     private int codigo;
+     private String Nombre;
+      private int Edad;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
+}
